55719c1cf1e471b4ff9414a58b180c8347b49cfc
Matthew Thomas <shrikegames@gmail.com>
2019-06-06 21:54:12 -0300

    Some minor clean-up. Added comments and some TODOs.
    Added a python requirements.txt file.

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,305 @@ use zip::result::ZipResult;
 use zip::write::{FileOptions, ZipWriter};
 const WALL_SPACING: f64 = 0.5;
 const MIN_PITCH: f64 = 10.0;
+
+fn main() -> std::io::Result<()> {
+	//	let mut file = File::open("src/song/ExpertPlus.json")?;
+	//	let mut contents = String::new();
+	//	file.read_to_string(&mut contents)?;
+	//	println!("{}", contents);
+
+	println!("Start map creation");
+
+	match create_bsaber_map() {
+		Ok(_) => {
+			println!("Map created");
+			println!("Start zipping");
+			fs::remove_file("/home/matt/Games/beatsaver-viewer-master/song.zip")?;
+			let mut file = File::create("/home/matt/Games/beatsaver-viewer-master/song.zip")
+				.expect("Couldn't create file");
+			create_zip_archive(&mut file).expect("Couldn't create archive");
+			println!("Finished zipping");
+		}
+		Err(e) => {
+			println!("Failed to create map. Error: {}", e);
+		}
+	}
+
+	Ok(())
+}
+/*
+* 
+*/
+fn create_bsaber_map() -> std::io::Result<()> {
+	fs::remove_file("src/song/ExpertPlus.json")?;
+	let mut file = File::create("src/song/ExpertPlus.json")?;
+	//TODO read from config or info.json
+	let version = "1.0.0";
+	let beats_per_minute: f64 = 150.0;
+	let beats_per_bar = 16;
+	let note_jump_speed = 15;
+	let shuffle = 1;
+	let shuffle_period = 0.1;
+	let time: f64 = 227.0;
+	//start of ExperPlus.json
+	let mut contents: String = format!(
+		"{{\"_version\": \"{}\",
+			\"_beatsPerMinute\": {},
+			\"_beatsPerBar\": {},
+			\"_noteJumpSpeed\": {},
+			\"_shuffle\": {},
+			\"_shufflePeriod\": {},
+			\"_time'\": {},
+			\"_songTimeOffset\": 0.0,
+			\"_notes\": [",
+		version, beats_per_minute, beats_per_bar, note_jump_speed, shuffle, shuffle_period, time
+	)
+	.to_owned();
+
+	//read the peak times and pitches
+	let peak_times_file = File::open("src/song/peak_times.txt")?;
+	let peak_times_buffer = BufReader::new(&peak_times_file);
+	//we'll keep track of times for the peaks and where walls might go
+	let mut peak_times = Vec::new();
+	let mut wall_times = Vec::new();
+	//keep the extracted pitch values too
+	let mut peak_pitches = Vec::new();
+	//find the highest pitch while we're at it
+	let mut highest_pitch: f64 = 0.0;
+	//iterate over the file line by line
+	for line in peak_times_buffer.lines() {
+		let line = line.unwrap();
+		let mut iter = line.split_whitespace();
+		//1.0216780045351475 175.67802
+		//first number is the time (in seconds), second number is the pitch measurement
+		let peak_time = iter.next().unwrap();
+		let peak_pitch = iter.next().unwrap();
+		//convert to floats
+		let peak_time_as_float = peak_time.parse::<f64>().unwrap();
+		let peak_pitch_as_float = peak_pitch.parse::<f64>().unwrap();
+		//store in vectors
+		peak_times.push(peak_time_as_float);
+		wall_times.push(peak_time_as_float);
+		peak_pitches.push(peak_pitch_as_float);
+		//track highest pitch
+		if peak_pitch_as_float > highest_pitch {
+			highest_pitch = peak_pitch_as_float;
+		}
+	}
+	//ensure the wall times always have the song time (length/duration)
+	wall_times.push(time);
+	//generate the map
+	contents = generate_map(
+		contents,
+		&peak_times,
+		&wall_times,
+		&peak_pitches,
+		highest_pitch,
+		beats_per_minute,
+		time,
+	);
+	//write it all out to file
+	file.write_all(contents.as_bytes())?;
+	drop(file);
+	Ok(())
+}
+/*
+* Generate a bsaber map using the information provided
+*/
+fn generate_map(
+	mut contents: String,
+	peak_times: &[f64],
+	wall_times: &[f64],
+	peak_pitches: &[f64],
+	highest_pitch: f64,
+	beats_per_minute: f64,
+	time: f64,
+) -> String {
+	//track which beat were on using an id
+	let mut id: usize = 0;
+	//track where we last placed any block
+	let mut last_x = 0;
+	let mut last_y = 0;
+	//track where we last placed a LEFT SABER block
+	let mut last_xl = 0;
+	let mut last_yl = 0;
+	//trakc where we last place a RIGHT saber block
+	let mut last_xr = 0;
+	let mut last_yr = 0;
+	//iterate over the peak times
+	for peak_time in peak_times {
+		//get the related pitch value
+		let peak_pitch = peak_pitches[id];
+		//now increase our id index
+		id += 1;
+		//if the pitch is high enough
+		if peak_pitch >= MIN_PITCH {
+			//value information:
+			//index = left  0-4  right
+			//layer = bottom  0-4  top
+			//note type = 0 left, 1 right, 2 bomb
+			//cut_direction = 0 up, 1 down, 2 left, 3 right, 4 NW, 5 NE, 6 SW, 7 SE, 8 omni-directional
+			//get information for a new note
+			let (time_beats, x, y, note_type, cut_direction, lrx, lry, lrxl, lryl, lrxr, lryr) =
+				get_note_information(
+					*peak_time,
+					peak_pitch,
+					highest_pitch,
+					beats_per_minute,
+					&last_x,
+					&last_y,
+					&last_xl,
+					&last_yl,
+					&last_xr,
+					&last_yr,
+				);
+			//update the last placed block positions	
+			last_x = lrx;
+			last_y = lry;
+			last_xl = lrxl;
+			last_yl = lryl;
+			last_xr = lrxr;
+			last_yr = lryr;
+			//create the note using the values we calculated
+			let note: String = create_note_json(
+				id,
+				*peak_time,
+				peak_pitch,
+				time_beats,
+				x,
+				y,
+				note_type,
+				cut_direction,
+			);
+			//add the note to the json string
+			contents.push_str(&note);
+		}
+	}
+	//generate walls
+	contents = generate_walls(contents, wall_times, &peak_pitches, beats_per_minute, time);
+	
+	
+	//return the completed json string
+	(contents)
+}
+/*
+* Generates walls to fill empty/low pitch areas
+*/
+fn generate_walls(
+	mut contents: String,
+	wall_times: &[f64],
+	peak_pitches: &[f64],
+	beats_per_minute: f64,
+	time: f64,
+) -> String {
+	//obstacles json string
+	let mut obstacles: String = "".to_string();
+	//id index
+	let mut id = 0;
+	//track the start time of a wall, we'll want it to start at the first quiet part
+	let mut start_time = 0.0;
+	//flag if we've found a wall start or not
+	let mut found_start = false;
+	//iterate over the wall times (including the last time added manually)
+	for peak_time in wall_times {
+		//wall times has an extra wall at the end added on so the size i 1 larger
+		let pitch = if id < peak_pitches.len() {
+			peak_pitches[id]
+		} else {
+			0.0
+		};
+		//increment the id index
+		id += 1;
+		//if it's quiet and we need a wall start
+		if pitch < MIN_PITCH && !found_start{
+			//silence is starting
+			//track the start time with a buffer
+			start_time = peak_time + WALL_SPACING;
+			found_start = true;
+		} else if found_start {
+			//peak occured so we can figure out the distance of the wall
+			//this is our wall end time, minus a buffer
+			let end_time = peak_time - WALL_SPACING;
+			//convert to beats (from seconds)
+			let time_beats = (start_time / 60.0) * beats_per_minute;
+			//println!("time_beats:{}", time_beats);
+			//specs for our wall
+			let mut line_index = 0;
+			let wall_type = 0;//0 vertical 1 horizontal?
+			let width = 0.5;
+			//calculate duration in number of beats (not seconds)
+			let duration = ((end_time - start_time) / 60.0) * beats_per_minute;
+			//println!("duration (beats):{}", duration);
+			//only make a wall if it's reasonably sized
+			if duration >= WALL_SPACING*2.0 {
+				//make left side wall
+				let wall: String = format!(
+					"{{
+							\"_time\": {},
+							\"_lineIndex\": {},
+							\"_type\": {},
+							\"_duration\": {},
+							\"_width\": {}
+						}},",
+					time_beats, line_index, wall_type, duration, width,
+				)
+				.to_owned();
+				//add to obstacles
+				obstacles.push_str(&wall);
+				//make right side wall
+				line_index = 3;
+				let wall2: String = format!(
+					"{{
+							\"_time\": {},
+							\"_lineIndex\": {},
+							\"_type\": {},
+							\"_duration\": {},
+							\"_width\": {}
+						}},",
+					time_beats, line_index, wall_type, duration, width,
+				)
+				.to_owned();
+				//add to obstacles
+				obstacles.push_str(&wall2);
+				//now free us to find another wall start position
+				found_start = false;
+				
+			}
+		}
+	}
+	
+	
+	//the end of the json file contents
+	//note adding an extra obstacle with no size so we don't have a hanging comma
+	let contents_end: String = format!(
+		"{{
+			\"_time\": {},
+			\"_lineIndex\": 3,
+			\"_lineLayer\": 3,
+			\"_type\": 1,
+			\"_cutDirection\": 0
+			}}],\"_events\": [],
+			\"_obstacles\": [{}
+			{{
+				\"_time\": 0,
+				\"_lineIndex\": 0,
+				\"_type\": 0,
+				\"_duration\": 1,
+				\"_width\": 1
+			}}]
+		}}",
+		time, obstacles
+	)
+	.to_owned();
+	//add to json string
+	contents.push_str(&contents_end);
+	(contents)
+}
+/*
+* Creates a note json string based on the date provided
+*/
 #[allow(clippy::too_many_arguments)]
-fn create_note(
+fn create_note_json(
 	id: usize,
 	peak_time: f64,
 	peak_pitch: f64,
@@ -39,6 +336,10 @@ fn create_note(
 	.to_owned();
 	(note)
 }
+/*
+* Get the information to place a new block based on logic.
+* Will include the bulk of the logic for making maps.
+*/
 #[allow(clippy::too_many_arguments, clippy::cyclomatic_complexity)]
 fn get_note_information(
 	peak_time: f64,
@@ -52,18 +353,33 @@ fn get_note_information(
 	last_xr: &i64,
 	last_yr: &i64,
 ) -> (f64, i64, i64, i64, i64, i64, i64, i64, i64, i64, i64) {
+	//pick randomly between left and right saber
 	let note_type = rand::thread_rng().gen_range(0, 2);
 	let mut cut_direction = 8;
 	let mut x: i64 = 0;
+	
 	if note_type == 0 {
 		//left
+		//start the note on the two left columns (randomly)
 		x = rand::thread_rng().gen_range(0, 2);
 	} else if note_type == 1 {
+		//right
+		//start the note on the two right columns (randomly)
 		x = rand::thread_rng().gen_range(2, 4);
 	}
+	//the y position/row is determined by comparing the pitch against the highest pitch note
+	//4 vertical rows
+	//TODO make 4 a constant
 	let mut y = ((peak_pitch / highest_pitch) * f64::from(4)) as i64;
+	//if we randomly picked the same coordinates as our last block (either saber)
+	//then we're going to adjust its position
 	while x == *last_x && y == *last_y {
+		//initially could be any cut direction (8 cardinal directions)
+		//cut_direction = 0 N, 1 S, 2 W, 3 E, 4 NW, 5 NE, 6 SW, 7 SE, 8 omni
+		//chosen at random
 		let random_direction = rand::thread_rng().gen_range(0, 8);
+		//based on the direction picked we'll change the direction of the noteblock
+		//this makes it easier to chain notes together for the player
 		if random_direction == 0 {
 			//N
 			y += 1;
@@ -116,10 +432,10 @@ fn get_note_information(
 			cut_direction = 8;
 		}
 	}
-	//cut_direction = 0 up, 1 down, 2 left, 3 right, 4 NW, 5 NE, 6 SW, 7 SE, 8 omni
+	//locally store the last note position
 	let mut lx = *last_x;
 	let mut ly = *last_x;
-
+	//if we have a left/right saber grab those specific values instead
 	if note_type == 0 {
 		lx = *last_xl;
 		ly = *last_yl;
@@ -128,29 +444,34 @@ fn get_note_information(
 		ly = *last_yr;
 	}
 
+	//compare to our last note position
+	//change the direction of the block based on our relative position
+	//if to the right point us in some right-ward cut direction
 	if x > lx {
+		
 		if y > ly {
-			cut_direction = 5;
+			cut_direction = 5;//NE
 		} else if y < ly {
-			cut_direction = 7;
+			cut_direction = 7;//SE
 		} else {
-			cut_direction = 3;
+			cut_direction = 3;//E
 		}
 	} else if x < lx {
 		if y > ly {
-			cut_direction = 4;
+			cut_direction = 4;//NW
 		} else if y < ly {
-			cut_direction = 6;
+			cut_direction = 6;//SW
 		} else {
-			cut_direction = 2;
+			cut_direction = 2;//W
 		}
 	} else if y > ly {
-		cut_direction = 0;
+		cut_direction = 0;//N
 	} else if y < ly {
-		cut_direction = 1;
+		cut_direction = 1;//S
 	}
 
 	//NOTE _time is IN BEATS, NOT SECONDS
+	//update our last used coordinates for notes
 	let time_beats = (peak_time / 60.0) * beats_per_minute;
 	let last_x_return = x;
 	let last_y_return = y;
@@ -158,7 +479,7 @@ fn get_note_information(
 	let mut last_yl_return = last_yl;
 	let mut last_xr_return = last_xr;
 	let mut last_yr_return = last_yr;
-
+	
 	if note_type == 0 {
 		last_xl_return = &x;
 		last_yl_return = &y;
@@ -166,7 +487,7 @@ fn get_note_information(
 		last_xr_return = &x;
 		last_yr_return = &y;
 	}
-
+	
 	(
 		time_beats,
 		x,
@@ -181,224 +502,11 @@ fn get_note_information(
 		*last_yr_return,
 	)
 }
-fn generate_map(
-	mut contents: String,
-	peak_times: &[f64],
-	wall_times: &[f64],
-	peak_pitches: &[f64],
-	highest_pitch: f64,
-	beats_per_minute: f64,
-	time: f64,
-) -> String {
-	let mut id: usize = 0;
-	let mut last_x = 0;
-	let mut last_y = 0;
-	let mut last_xl = 0;
-	let mut last_yl = 0;
-	let mut last_xr = 0;
-	let mut last_yr = 0;
 
-	for peak_time in peak_times {
-		let peak_pitch = peak_pitches[id];
-
-		id += 1;
-		if peak_pitch >= MIN_PITCH {
-			//index = left  0-4  right
-			//layer = bottom  0-4  top
-			//note type = 0 left, 1 right, 2 bomb
-			//cut_direction = 0 up, 1 down, 2 left, 3 right, 4 NW, 5 NE, 6 SW, 7 SE, 8 omni
-
-			let (time_beats, x, y, note_type, cut_direction, lrx, lry, lrxl, lryl, lrxr, lryr) =
-				get_note_information(
-					*peak_time,
-					peak_pitch,
-					highest_pitch,
-					beats_per_minute,
-					&last_x,
-					&last_y,
-					&last_xl,
-					&last_yl,
-					&last_xr,
-					&last_yr,
-				);
-			last_x = lrx;
-			last_y = lry;
-			last_xl = lrxl;
-			last_yl = lryl;
-			last_xr = lrxr;
-			last_yr = lryr; //12sec at 130bpm =? beats
-				//0.2minutes*130=26
-			let note: String = create_note(
-				id,
-				*peak_time,
-				peak_pitch,
-				time_beats,
-				x,
-				y,
-				note_type,
-				cut_direction,
-			);
-
-			contents.push_str(&note);
-		}
-	}
-	contents = generate_walls(contents, wall_times, &peak_pitches, beats_per_minute, time);
-	(contents)
-}
-fn generate_walls(
-	mut contents: String,
-	wall_times: &[f64],
-	peak_pitches: &[f64],
-	beats_per_minute: f64,
-	time: f64,
-) -> String {
-	let mut obstacles: String = "".to_string();
-	let mut id = 0;
-	let mut start_time = 0.0;
-	let mut found_start = false;
-	for peak_time in wall_times {
-		//wall times has an extra wall at the end added on so the size i 1 larger
-		let pitch = if id < peak_pitches.len() {
-			peak_pitches[id]
-		} else {
-			0.0
-		};
-		id += 1;
-
-		if pitch < MIN_PITCH && !found_start{
-			//silence is starting
-			start_time = peak_time + WALL_SPACING;
-			found_start = true;
-		} else if found_start {
-			//peak occured so we can figure out the distance of the wall
-			let end_time = peak_time - WALL_SPACING;
-			let time_beats = (start_time / 60.0) * beats_per_minute;
-			//println!("time_beats:{}", time_beats);
-			//let random_side = rand::thread_rng().gen_range(0, 2);
-			let mut line_index = 0;
-			let wall_type = 0;
-			let width = 0.5;
-			let duration = ((end_time - start_time) / 60.0) * beats_per_minute;
-			//println!("duration (beats):{}", duration);
-			//only make a wall if it's reasonably sized
-			if duration >= WALL_SPACING*2.0 {
-				let wall: String = format!(
-					"{{
-							\"_time\": {},
-							\"_lineIndex\": {},
-							\"_type\": {},
-							\"_duration\": {},
-							\"_width\": {}
-						}},",
-					time_beats, line_index, wall_type, duration, width,
-				)
-				.to_owned();
-				obstacles.push_str(&wall);
-				line_index = 3;
-				let wall2: String = format!(
-					"{{
-							\"_time\": {},
-							\"_lineIndex\": {},
-							\"_type\": {},
-							\"_duration\": {},
-							\"_width\": {}
-						}},",
-					time_beats, line_index, wall_type, duration, width,
-				)
-				.to_owned();
-				obstacles.push_str(&wall2);
-				found_start = false;
-				
-			}
-		}
-	}
-	let contents_end: String = format!(
-		"{{
-			\"_time\": {},
-			\"_lineIndex\": 3,
-			\"_lineLayer\": 3,
-			\"_type\": 1,
-			\"_cutDirection\": 0
-			}}],\"_events\": [],
-			\"_obstacles\": [{}
-			{{
-				\"_time\": 0,
-				\"_lineIndex\": 0,
-				\"_type\": 0,
-				\"_duration\": 1,
-				\"_width\": 1
-			}}]
-		}}",
-		time, obstacles
-	)
-	.to_owned();
-	contents.push_str(&contents_end);
-	(contents)
-}
-fn create_song() -> std::io::Result<()> {
-	fs::remove_file("src/song/ExpertPlus.json")?;
-	let mut file = File::create("src/song/ExpertPlus.json")?;
-	let version = "1.0.0";
-	let beats_per_minute: f64 = 150.0;
-	let beats_per_bar = 16;
-	let note_jump_speed = 15;
-	let shuffle = 1;
-	let shuffle_period = 0.1;
-	let time: f64 = 227.0;
-
-	let mut contents: String = format!(
-		"{{\"_version\": \"{}\",
-			\"_beatsPerMinute\": {},
-			\"_beatsPerBar\": {},
-			\"_noteJumpSpeed\": {},
-			\"_shuffle\": {},
-			\"_shufflePeriod\": {},
-			\"_time'\": {},
-			\"_songTimeOffset\": 0.0,
-			\"_notes\": [",
-		version, beats_per_minute, beats_per_bar, note_jump_speed, shuffle, shuffle_period, time
-	)
-	.to_owned();
-
-	let peak_times_file = File::open("src/song/peak_times.txt")?;
-	let peak_times_buffer = BufReader::new(&peak_times_file);
-
-	let mut peak_times = Vec::new();
-	let mut wall_times = Vec::new();
-	let mut peak_pitches = Vec::new();
-	let mut highest_pitch: f64 = 0.0;
-	for line in peak_times_buffer.lines() {
-		let line = line.unwrap();
-		let mut iter = line.split_whitespace();
-		let peak_time = iter.next().unwrap();
-		let peak_pitch = iter.next().unwrap();
-
-		let peak_time_as_float = peak_time.parse::<f64>().unwrap();
-		let peak_pitch_as_float = peak_pitch.parse::<f64>().unwrap();
-
-		peak_times.push(peak_time_as_float);
-		wall_times.push(peak_time_as_float);
-		peak_pitches.push(peak_pitch_as_float);
-		if peak_pitch_as_float > highest_pitch {
-			highest_pitch = peak_pitch_as_float;
-		}
-	}
-	wall_times.push(time);
-
-	contents = generate_map(
-		contents,
-		&peak_times,
-		&wall_times,
-		&peak_pitches,
-		highest_pitch,
-		beats_per_minute,
-		time,
-	);
-
-	file.write_all(contents.as_bytes())?;
-	drop(file);
-	Ok(())
-}
+/*
+* Zip up the important files from /src/song
+* This is the zip beat saber, bsaber, or the map previewer can use
+ */
 fn create_zip_archive<T: Seek + Write>(buf: &mut T) -> ZipResult<()> {
 	let mut writer = ZipWriter::new(buf);
 	println!("Adding ExpertPlus");
@@ -421,28 +529,4 @@ fn create_zip_archive<T: Seek + Write>(buf: &mut T) -> ZipResult<()> {
 	writer.finish()?;
 	Ok(())
 }
-fn main() -> std::io::Result<()> {
-	//	let mut file = File::open("src/song/ExpertPlus.json")?;
-	//	let mut contents = String::new();
-	//	file.read_to_string(&mut contents)?;
-	//	println!("{}", contents);
 
-	println!("Start map creation");
-
-	match create_song() {
-		Ok(_) => {
-			println!("Map created");
-			println!("Start zipping");
-			fs::remove_file("/home/matt/Games/beatsaver-viewer-master/song.zip")?;
-			let mut file = File::create("/home/matt/Games/beatsaver-viewer-master/song.zip")
-				.expect("Couldn't create file");
-			create_zip_archive(&mut file).expect("Couldn't create archive");
-			println!("Finished zipping");
-		}
-		Err(e) => {
-			println!("Failed to create map. Error: {}", e);
-		}
-	}
-
-	Ok(())
-}
